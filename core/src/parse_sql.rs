3d417e2b753866191a74594607da750c9cf3af48
Jiseok CHOI <jiseok.dev@gmail.com>
2022-10-10 13:01:50 +0900

    done

--- core/src/parse_sql.rs
+++ core/src/parse_sql.rs
@@ -18,124 +18,70 @@ pub fn parse<Sql: AsRef<str>>(sql: Sql) -> Result<Vec<SqlStatement>> {
     Parser::parse_sql(&DIALECT, sql.as_ref()).map_err(|e| Error::Parser(format!("{:#?}", e)))
 }
 
-pub fn parse_query<Sql: AsRef<str>>(sql_expr: Sql) -> Result<SqlQuery> {
-    let tokens = Tokenizer::new(&DIALECT, sql_expr.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
+macro_rules! impl_parse_method {
+    ($method_name: ident, $return_type: ty) => {
+        pub fn $method_name<Sql: AsRef<str>>(sql: Sql) -> Result<$return_type> {
+            let tokens = Tokenizer::new(&DIALECT, sql.as_ref())
+                .tokenize()
+                .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
 
-    Parser::new(tokens, &DIALECT)
-        .parse_query()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
+            Parser::new(tokens, &DIALECT)
+                .$method_name()
+                .map_err(|e| Error::Parser(format!("{:#?}", e)))
+        }
+    };
+
+    ($method_name: ident, $parser_method: ident, $return_type: ty) => {
+        pub fn $method_name<Sql: AsRef<str>>(sql: Sql) -> Result<$return_type> {
+            let tokens = Tokenizer::new(&DIALECT, sql.as_ref())
+                .tokenize()
+                .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
+
+            Parser::new(tokens, &DIALECT)
+                .$parser_method()
+                .map_err(|e| Error::Parser(format!("{:#?}", e)))
+        }
+    };
+
+    ($method_name: ident, $parser_method: ident, $parser_arg: path, $return_type: ty) => {
+        pub fn $method_name<Sql: AsRef<str>>(sql: Sql) -> Result<$return_type> {
+            let tokens = Tokenizer::new(&DIALECT, sql.as_ref())
+                .tokenize()
+                .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
+
+            Parser::new(tokens, &DIALECT)
+                .$parser_method($parser_arg)
+                .map_err(|e| Error::Parser(format!("{:#?}", e)))
+        }
+    };
 }
 
-pub fn parse_expr<Sql: AsRef<str>>(sql_expr: Sql) -> Result<SqlExpr> {
-    let tokens = Tokenizer::new(&DIALECT, sql_expr.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
+impl_parse_method!(parse_query, SqlQuery);
+impl_parse_method!(parse_expr, SqlExpr);
+impl_parse_method!(parse_select_item, SqlSelectItem);
+impl_parse_method!(parse_order_by_expr, SqlOrderByExpr);
+impl_parse_method!(parse_column_def, SqlColumnDef);
+impl_parse_method!(parse_data_type, SqlDataType);
+impl_parse_method!(parse_assignment, SqlAssignment);
+impl_parse_method!(parse_identifiers, Vec<SqlIdent>);
 
-    Parser::new(tokens, &DIALECT)
-        .parse_expr()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
+impl_parse_method!(parse_interval, SqlExpr);
 
-pub fn parse_comma_separated_exprs<Sql: AsRef<str>>(sql_exprs: Sql) -> Result<Vec<SqlExpr>> {
-    let tokens = Tokenizer::new(&DIALECT, sql_exprs.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_comma_separated(Parser::parse_expr)
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
-
-pub fn parse_select_item<Sql: AsRef<str>>(sql_select_item: Sql) -> Result<SqlSelectItem> {
-    let tokens = Tokenizer::new(&DIALECT, sql_select_item.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_select_item()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
-
-pub fn parse_select_items<Sql: AsRef<str>>(sql_select_items: Sql) -> Result<Vec<SqlSelectItem>> {
-    let tokens = Tokenizer::new(&DIALECT, sql_select_items.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_comma_separated(Parser::parse_select_item)
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
-
-pub fn parse_interval<Sql: AsRef<str>>(sql_interval: Sql) -> Result<SqlExpr> {
-    let tokens = Tokenizer::new(&DIALECT, sql_interval.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_interval()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
-
-pub fn parse_order_by_expr<Sql: AsRef<str>>(sql_order_by_expr: Sql) -> Result<SqlOrderByExpr> {
-    let tokens = Tokenizer::new(&DIALECT, sql_order_by_expr.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_order_by_expr()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
-
-pub fn parse_order_by_exprs<Sql: AsRef<str>>(
-    sql_orderby_exprs: Sql,
-) -> Result<Vec<SqlOrderByExpr>> {
-    let tokens = Tokenizer::new(&DIALECT, sql_orderby_exprs.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_comma_separated(Parser::parse_order_by_expr)
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
-
-pub fn parse_column_def<Sql: AsRef<str>>(sql_column_def: Sql) -> Result<SqlColumnDef> {
-    let tokens = Tokenizer::new(&DIALECT, sql_column_def.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_column_def()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
-
-pub fn parse_data_type<Sql: AsRef<str>>(sql_data_type: Sql) -> Result<SqlDataType> {
-    let tokens = Tokenizer::new(&DIALECT, sql_data_type.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_data_type()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
-
-pub fn parse_sql_assignment<Sql: AsRef<str>>(sql_assignment: Sql) -> Result<SqlAssignment> {
-    let tokens = Tokenizer::new(&DIALECT, sql_assignment.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_assignment()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
-
-pub fn parse_identifiers<Sql: AsRef<str>>(sql_identifiers: Sql) -> Result<Vec<SqlIdent>> {
-    let tokens = Tokenizer::new(&DIALECT, sql_identifiers.as_ref())
-        .tokenize()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))?;
-
-    Parser::new(tokens, &DIALECT)
-        .parse_identifiers()
-        .map_err(|e| Error::Parser(format!("{:#?}", e)))
-}
+impl_parse_method!(
+    parse_comma_separated_exprs,
+    parse_comma_separated,
+    Parser::parse_expr,
+    Vec<SqlExpr>
+);
+impl_parse_method!(
+    parse_select_items,
+    parse_comma_separated,
+    Parser::parse_select_item,
+    Vec<SqlSelectItem>
+);
+impl_parse_method!(
+    parse_order_by_exprs,
+    parse_comma_separated,
+    Parser::parse_order_by_expr,
+    Vec<SqlOrderByExpr>
+);
